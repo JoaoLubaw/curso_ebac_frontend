568a0f3d07bd0340fe53c2b57555a08bdb09cce8
João Lubawaski <jlubawaski@gmail.com>
2023-04-05 20:07:14 -0300

    Exercicio de jQuery

--- main.js
+++ main.js
@@ -1,42 +1,11 @@
-const form = document.getElementById ('form')
-let maior = false;
-
-function validacao (A, B){
-    return A < B;
-}
-
-form.addEventListener ('submit', function(e) 
-{
+$('form').on('submit', function(e) {
     e.preventDefault();
-    const CampoA = document.getElementById('A')
-    const CampoB = document.getElementById('B')
-    const NA = document.getElementById('A').value
-    const NB = document.getElementById('B').value
-    const A = parseFloat(NA);
-    const B = parseFloat(NB);
-    const Mensagemsucesso = `Parabéns, a resposta é sim, os desenhos nos ensinaram direitinho e hoje você sabe que <b>${A}</b> é menor que <b>${B}</b>...A não ser que... Você não é o ChatGpt...né?`;
-
-    maior=validacao(A,B);
-
-    if(maior == true){
-        const centersucesso = document.querySelector('.sucesso');
-        document.querySelector('.erro').style.display ='none';
-        centersucesso.innerHTML = Mensagemsucesso;
-        centersucesso.style.display = "block"
-        setTimeout(ce)
-
-        CampoA.value = ' ';
-        CampoB.value = ' ';
-    }
-    else{
-        const centersucesso = document.querySelector('.sucesso');
-        centersucesso.style.display = "none"
-        document.querySelector('.erro').style.display ='block';
-    }
-
+    const tarefa = $('#tarefa').val();
+    const novo = $(`<li id="item">${tarefa}</li>`)
+    $(novo).appendTo('ul');
+    $('#tarefa').val('');
 })
 
-
-
-
-    
+$('ul').on('click', 'li', function() {
+    $(this).addClass('feito');
+});
